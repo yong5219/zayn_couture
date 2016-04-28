e54cb6e29787b91260c9951e9068dbe275131566
TING JIA YONG <yong_5219@hotmail.com>
2016-04-28 16:22:58 +0800

    update

--- shopping_project/shopping/orders/urls.py
+++ shopping_project/shopping/orders/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     'orders.views',
     url(r'^checkout/$', 'checkout', name='order_checkout'),
+    url(r'^checkout-postback/$', 'checkout_postback', name='order_checkout_postback'),
 )
--- shopping_project/shopping/orders/views.py
+++ shopping_project/shopping/orders/views.py
@@ -23,17 +23,50 @@ def checkout(request):
         if request.method == 'POST':
             form = CheckOutForm(request.POST, initial={'cart': order_cart.pk, 'user': order_cart.owner.pk, })
             if form.is_valid():
-                # payload = {'MerchantCode': 'M03228', 'PaymentId': '', 'RefNo': 'A00000001', 'Amount': '1.00', 'Currency': 'MYR', 'ProdDesc': 'clothing product',
-                #             'UserName': 'testinguser', 'UserEmail': 'yong_5219@hotmail.com', 'UserContact': '0162926391', 'Remark': '', 'Lang': 'UTF-8', 'Signature': '84dNMbfgjLMS42IqSTPqQ99cUGA',
-                #             'ResponseURL': 'http://zayncouture.webfactional.com/order/checkout/', 'BackendURL': ''}
-                # r = requests.get('https://www.mobile88.com/ePayment/entry.asp', params=payload)
-                # return HttpResponseRedirect(r.url)
-
-                form.save(user=request.user, cart=order_cart)
-                messages.success(request, u'Order had been successful.')
-                return redirect('home')
+                payload = {'MerchantCode': 'M03228', 'PaymentId': '', 'RefNo': 'A00000001', 'Amount': '1.00', 'Currency': 'MYR', 'ProdDesc': 'clothing product',
+                            'UserName': 'testinguser', 'UserEmail': 'yong_5219@hotmail.com', 'UserContact': '0162926391', 'Remark': '', 'Lang': 'UTF-8', 'Signature': '84dNMbfgjLMS42IqSTPqQ99cUGA',
+                            'ResponseURL': 'http://zayncouture.webfactional.com/order/checkout-postback/', 'BackendURL': ''}
+                r = requests.get('https://www.mobile88.com/ePayment/entry.asp', params=payload)
+                return HttpResponseRedirect(r.url)
+                # form.save(user=request.user, cart=order_cart)
+                # messages.success(request, u'Order had been successful.')
+                # return redirect('home')
             else:
                 messages.error(request, u'Order failed. Please try again!')
+                # print form.errors
+    else:
+        messages.error(request, u'Your have no product in your order cart.')
+        return redirect('cart_list')
+    context = {
+        'form': form,
+        'order_cart': order_cart,
+    }
+
+    return render_to_response('order/checkout.html', context, RequestContext(request))
+
+
+@login_required
+def checkout_postback(request):
+    order_cart = OrderCart.objects.get_current_cart(request.user)
+    if order_cart.lines.count() > 0:
+        form = CheckOutForm(initial={'cart': order_cart.pk, 'user': order_cart.owner.pk, })
+
+        if request.method == 'POST':
+            form = CheckOutForm(request.POST, initial={'cart': order_cart.pk, 'user': order_cart.owner.pk, })
+            if form.is_valid():
+
+                status = request.POST['Status']
+
+                if status == "1":
+                    form.save(user=request.user, cart=order_cart)
+                    messages.success(request, u'Order had been successful.')
+                    return redirect('home')
+                else:
+                    messages.error(request, u'Order failed. Please try again!')
+                return redirect('order_checkout')
+            else:
+                messages.error(request, u'Order failed. Please try again!')
+                return redirect('order_checkout')
                 # print form.errors
     else:
         messages.error(request, u'Your have no product in your order cart.')
