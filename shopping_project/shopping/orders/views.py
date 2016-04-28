ec77c64b6ea0772f88809b806fdea43724e6792d
TING JIA YONG <yong_5219@hotmail.com>
2016-04-28 16:41:52 +0800

    update

--- shopping_project/shopping/orders/views.py
+++ shopping_project/shopping/orders/views.py
@@ -23,9 +23,7 @@ def checkout(request):
         if request.method == 'POST':
             form = CheckOutForm(request.POST, initial={'cart': order_cart.pk, 'user': order_cart.owner.pk, })
             if form.is_valid():
-                payload = {'MerchantCode': 'M03228', 'PaymentId': '', 'RefNo': 'A00000001', 'Amount': '1.00', 'Currency': 'MYR', 'ProdDesc': 'clothing product',
-                            'UserName': 'testinguser', 'UserEmail': 'yong_5219@hotmail.com', 'UserContact': '0162926391', 'Remark': '', 'Lang': 'UTF-8', 'Signature': '84dNMbfgjLMS42IqSTPqQ99cUGA',
-                            'ResponseURL': 'http://zayncouture.webfactional.com/order/checkout-postback/', 'BackendURL': ''}
+                payload = {'MerchantCode': 'M03228', 'PaymentId': '', 'RefNo': 'A00000001', 'Amount': '1.00', 'Currency': 'MYR', 'ProdDesc': 'clothing product', 'UserName': 'testinguser', 'UserEmail': 'yong_5219@hotmail.com', 'UserContact': '0162926391', 'Remark': '', 'Lang': 'UTF-8', 'Signature': '84dNMbfgjLMS42IqSTPqQ99cUGA', 'ResponseURL': 'http://zayncouture.webfactional.com/order/checkout-postback/', 'BackendURL': ''}
                 r = requests.get('https://www.mobile88.com/ePayment/entry.asp', params=payload)
                 return HttpResponseRedirect(r.url)
                 # form.save(user=request.user, cart=order_cart)
@@ -52,6 +50,7 @@ def checkout_postback(request):
         form = CheckOutForm(initial={'cart': order_cart.pk, 'user': order_cart.owner.pk, })
 
         if request.method == 'POST':
+            return redirect('home')
             form = CheckOutForm(request.POST, initial={'cart': order_cart.pk, 'user': order_cart.owner.pk, })
             if form.is_valid():
 
